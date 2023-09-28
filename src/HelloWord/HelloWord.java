5f0b89b8ca59d783f69ea4305d810b1214816d7d
Maria <marry.parkhomchuk@gmail.com>
2023-09-28 13:28:55 +0300

    First task : "Hello Word"

---
+++ src/HelloWord/HelloWord.java
@@ -0,0 +1,8 @@
+package HelloWord;
+
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello Word!");
+        System.out.println("My name is Maria Parkhomchuk. Welcom to my first Java project");
+    }
+}
